c11936f0f094f041699e82ecc0800fe9c1923da8
Eldos-05 <eldos.220505@gmail.com>
2025-05-16 16:55:43 +0600

    Initial commit

---
+++ src/main/java/comsep_23/JetEco/JetEcoApplication.java
@@ -0,0 +1,13 @@
+package comsep_23.JetEco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetEcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetEcoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comsep_23/JetEco/JetEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package comsep_23.JetEco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetEcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
