c479db89aa30eb90a1528b9b4d4d000bfa283c57
UlanAbdykerimov <150321228+UlanAbdykerimov@users.noreply.github.com>
2025-05-20 18:52:17 +0600

    Registration page is added

--- src/main/java/comsep_23/JetEco/controller/RegistrationPageController.java
+++ src/main/java/comsep_23/JetEco/controller/RegistrationPageController.java
@@ -1,16 +1,25 @@
 package comsep_23.JetEco.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegistrationPageController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("title", "Добро пожаловать на сайт!");
-        return "index"; // ищет шаблон register.html в папке templates
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register"; // templates/register.html
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login"; // templates/login.html
+    }
+
+    @GetMapping("/business-signup")
+    public String showBusinessSignupPage() {
+        return "business-signup"; // templates/business-signup.html
     }
 }
+
 
